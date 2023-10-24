ebdd1229270c03f6e4618094d7732b9090dc143d
mutiarazulfina <148845761+mutiarazulfina@users.noreply.github.com>
2023-10-23 18:27:10 -1200

    Add files via upload

---
+++ latihan1b.php
@@ -0,0 +1,23 @@
+<?php
+
+//Menetapkan Nilai
+$n = 10;
+echo "Aku adalah angka $n </br>";
+
+//Perkalian
+$hasilkali = $n * 8; //Tanda * adalah operator
+echo "Jika aku dikali 8, jumlahku sekarang 80 = $hasilkali </br>";
+
+//Pembagian
+$hasilbagi = $hasilkali / 4; //Tanda / adalah operator
+echo "Jika aku dibagi 4, jumlahku sekarang 20 = $hasilbagi </br>";
+
+//Pengurangan
+$hasilkurang = $hasilbagi - 6; //Tanda - adalah operator
+echo "Jika aku dikurang 6, jumlahku sekarang 14 = $hasilkurang </br>";
+
+//Penambahan
+$hasiltambah = $hasilkurang + 2; //Tanda + adalah operator
+echo "Jika aku ditambah 2, jumlahku sekarang 12 = $hasiltambah </br>";
+
+?>
